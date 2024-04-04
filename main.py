cbbdab527b10bb1d4d853d23ceabbdc2350130c3
imshubhy <warmachinegaming4@gmail.com>
2024-04-05 01:52:33 +0530

    remove encryption generated features for interface

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import os
 
 
 # Function to generate and save encryption key
-
-# encryption key required for any operation e.g. -> encrypting and decrypting
 def generate_key():
-    key = Fernet.generate_key()                # generating the key here
-    with open("key.key", "wb") as key_file:    # creating key file
-        key_file.write(key)                    # saving the key into key_file
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+    print("Encryption key generated and saved successfully.")
 
 
 # Function to load encryption key
-
-def load_key():          # need to have the key for encrypting and decrypting the password.
+def load_key():
     if not os.path.exists("key.key"):
         print("Encryption key not found. Generating a new key...")
         generate_key()
@@ -23,7 +21,7 @@ def load_key():          # need to have the key for encrypting and decrypting th
 
 # Function to encrypt password
 def encrypt_password(password, key):
-    cipher_suite = Fernet(key)       # object
+    cipher_suite = Fernet(key)
     return cipher_suite.encrypt(password.encode()).decode()
 
 
@@ -38,8 +36,8 @@ def view_passwords(key):
     try:
         with open('passwords.txt', 'r') as f:
             for line in f.readlines():
-                data = line.rstrip()   # removing trailing space
-                user, encrypted_pass = data.split("|")   # getting user and password from txt
+                data = line.rstrip()  # removing trailing space
+                user, encrypted_pass = data.split("|")  # getting user and password from txt
                 decrypted_pass = decrypt_password(encrypted_pass, key)
                 print("User:", user, "| Password:", decrypted_pass)
     except FileNotFoundError:
@@ -63,8 +61,7 @@ def main():
         print("\nPassword Manager Menu:")
         print("1. View Passwords")
         print("2. Add Password")
-        print("3. Generate New Encryption Key")
-        print("4. Quit")
+        print("3. Quit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
@@ -72,9 +69,6 @@ def main():
         elif choice == "2":
             add_password(key)
         elif choice == "3":
-            generate_key()
-            key = load_key()
-        elif choice == "4":
             print("Exiting Password Manager. Goodbye!")
             break
         else:
